ca0aa80b4156ba2d246c5513f8f1a1805e7c043d
Abayraj <112469448+Abayraj@users.noreply.github.com>
2023-03-30 22:00:18 +0530

    chanel-clone

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    
+    extend: {
+      colors: {
+        thapa:{
+          400:"#333333",
+
+        },
+
+    },
+    },
+  },
+  plugins: [],
+}
